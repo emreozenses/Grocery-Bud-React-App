ca2a88e6bd39a997722aff97106a4295c3e7fedb
Emre OZENSES <emreozenses@gmail.com>
2024-05-10 18:13:07 +0300

    state hooks, form component and addItem function completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import { isValidElement, useState } from "react";
+import Form from "./Form";
+import { ToastContainer, toast } from 'react-toastify'
+import { nanoid } from "nanoid";
+
+
 const App = () => {
-  return <h2>Grocery Bud - Starter</h2>;
-};
+
+const [items,setItems] = useState([]);
+const addItem = (item)=>{
+ if (item!==''){
+  
+  try {
+    
+      const newItems = [...items,{name:item,prop:false,id:nanoid()}];
+      setItems(newItems);
+      
+      toast.success('success')
+    
+    
+  } catch (error) {
+      toast.error('cannot add item to the list')
+    
+  }
+} else {
+  toast.error('please insert your item')
+
+}}
+
+console.log(items);
+  return (
+  <section className="section-center">
+    <ToastContainer className="top-center"/>
+    <Form items={items} addItem={addItem} setItems={setItems}/>
+    </section>
+)};
 
 export default App;
